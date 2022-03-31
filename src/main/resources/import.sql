33152697b53f9cc308b9799fac59ad72bb8ff0ef
ppires <pe8542@gmail.com>
2022-03-30 21:40:54 -0300

    Adding build docker image capabilities

--- src/main/resources/import.sql
+++
@@ -1,6 +0,0 @@
-insert into channel(id, youtube_channel_id, count) values (100, 'UCQU13KSYd2xepIrAzHUa7mA', 10);
-insert into task (id, processed, youtube_channel_id, status, LAST_UPDATE) values (1, 0, 'UCQU13KSYd2xepIrAzHUa7mA', 'ERROR', current_timestamp);
-insert into task (id, processed, youtube_channel_id, status, LAST_UPDATE) values (2, 0, 'UCSBp6wFCG4bVtk1TUlMyIQQ', 'ERROR', current_timestamp);
-insert into task (id, processed, youtube_channel_id, status, LAST_UPDATE) values (3, 0, 'UCPiqjJb6lwdFrAdXjtC0XSQ', 'ERROR', current_timestamp);
-insert into task (id, processed, youtube_channel_id, status, LAST_UPDATE) values (4, 0, 'UCZvysOTYDHMewYyIX_at8mw', 'ERROR', current_timestamp);
-insert into task (id, processed, youtube_channel_id, status, LAST_UPDATE) values (5, 0, 'UCkrm2zh05lRyYdWHpK0p45Q', 'PROCESSING', current_timestamp);
